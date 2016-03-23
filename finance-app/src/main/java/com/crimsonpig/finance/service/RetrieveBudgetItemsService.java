22b27d249e547a76de156452789c1a86525db227
crimsonpig <keith.stabbert@yahoo.com>
2016-03-22 22:03:43 -0700

    Correcting defect

--- finance-app/src/main/java/com/crimsonpig/finance/service/RetrieveBudgetItemsService.java
+++ finance-app/src/main/java/com/crimsonpig/finance/service/RetrieveBudgetItemsService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
 import com.crimsonpig.finance.api.PlannedBudgetItem;
@@ -11,12 +13,14 @@ import com.crimsonpig.finance.budget.BudgetItem;
 import com.crimsonpig.finance.mapper.BudgetItemMapper;
 
 public class RetrieveBudgetItemsService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(RetrieveBudgetItemsService.class);
 
 	private static final String HOST = "linuxbox";
 	
 	private static final String PATH = HOST + "/budget";
 	
-	private static final String URL_TEMPLATE = "http://" + PATH + " ?startDt=%s&endDt=%s";
+	private static final String URL_TEMPLATE = "http://" + PATH + "?startDt=%s&endDt=%s";
 	
 	private RestTemplate restTemplate;
 	
@@ -30,6 +34,7 @@ public class RetrieveBudgetItemsService {
 	public List<BudgetItem> retrieveBudgetItems(String startDt, String endDt){
 
 		String budgetUrl = String.format(URL_TEMPLATE, startDt, endDt);
+
 		PlannedBudgetItem[] budgetItems = restTemplate.getForObject(budgetUrl, PlannedBudgetItem[].class);
 
 		return Stream.of(budgetItems)
