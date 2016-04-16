4be986b350dc8c832c28d736ca3490a18387c6d8
crimsonpig <keith.stabbert@yahoo.com>
2016-04-16 00:20:47 -0700

    Cleaning up BudgetItemSpecification

--- finance-app/src/main/java/com/crimsonpig/finance/entity/BudgetItemSpecification.java
+++ finance-app/src/main/java/com/crimsonpig/finance/entity/BudgetItemSpecification.java
@@ -2,6 +2,8 @@ package com.crimsonpig.finance.entity;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -15,19 +17,28 @@ public class BudgetItemSpecification {
 //    .filter(_.startDate <= params.endDt)
 //    .filter(_.endDate >= params.startDt))
 
+	private static final Predicate[] predArr = new Predicate[0];
+	
 	public static Specification<BudgetItemEntity> findBudgetItems(LocalDate startDate, LocalDate endDate, String category){
+
 		return new Specification<BudgetItemEntity>(){
 
 			public Predicate toPredicate(Root<BudgetItemEntity> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
 				
+				List<Predicate> predicates = new ArrayList<Predicate>(3);
+				
 				Predicate startDatePredicate = builder.lessThanOrEqualTo(root.get("startDate"), Date.valueOf(endDate));
 				Predicate endDatePredicate = builder.greaterThanOrEqualTo(root.get("endDate"), Date.valueOf(startDate));
-				if(category == null){
-					return builder.and(startDatePredicate, endDatePredicate);
-				} else {
-				Predicate categoryPredicate = builder.equal(root.get("category"), category);
-					return builder.and(startDatePredicate, endDatePredicate, categoryPredicate);
-				}
+				predicates.add(startDatePredicate);
+				predicates.add(endDatePredicate);
+				
+				if(category != null){
+					Predicate categoryPredicate = builder.equal(root.get("category"), category);
+					predicates.add(categoryPredicate);
+				} 
+				
+				return builder.and(predicates.toArray(predArr));
+				
 			}
 			
 		};
