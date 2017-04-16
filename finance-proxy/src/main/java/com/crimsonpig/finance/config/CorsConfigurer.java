2872d8446e149f17cc9c0dddfe354202ec7fac8f
crimsonpig <keith.stabbert@yahoo.com>
2017-04-15 23:51:28 -0700

    Getting CORS to work in finance-proxy

---
+++ finance-proxy/src/main/java/com/crimsonpig/finance/config/CorsConfigurer.java
@@ -0,0 +1,28 @@
+package com.crimsonpig.finance.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfigurer {
+
+	@Value("${api.allowedorigins}")
+	private String allowedOrigins;
+	
+	
+	@Bean
+	public CorsFilter corsFilter(){
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+		source.registerCorsConfiguration("/**", configuration);
+		return new CorsFilter(source);
+	}
+}
