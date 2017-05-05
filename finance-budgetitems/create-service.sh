23fe361d3a2bf528300de8d02fa7640beac0c79a
crimsonpig <keith.stabbert@gmail.com>
2017-05-04 22:06:51 -0700

    Creating a Linux service for finance-budgetitems

---
+++ finance-budgetitems/create-service.sh
@@ -0,0 +1,4 @@
+cp finance-budgetitems.service /etc/systemd/system
+sudo systemctl enable finance-budgetitems.service
+sudo systemctl start finance-budgetitems.service
+
---
+++ finance-budgetitems/deploy.sh
@@ -0,0 +1,3 @@
+sudo cp target/finance-budgetitems-1.0.0-SNAPSHOT.jar /opt/finance-budgetitems.jar
+sudo systemctl stop finance-budgetitems.service
+sudo systemctl start finance-budgetitems.service
