48257ca10c803497152d6f26773ed376da519a2a
crimsonpig <keith.stabbert@yahoo.com>
2017-05-11 19:12:52 -0700

    Adding build shell script

---
+++ build.sh
@@ -0,0 +1,11 @@
+cd ~/git/finance-app/finance-domain
+mvn clean install
+cd ~/git/finance-app/finance-budgetitems
+mvn clean install
+cd ~/git/finance-app/finance-app
+mvn clean install
+cd ~/git/finance-app/finance-compare
+mvn clean install
+cd ~/git/finance-app/finance-proxy
+mvn clean install
+
