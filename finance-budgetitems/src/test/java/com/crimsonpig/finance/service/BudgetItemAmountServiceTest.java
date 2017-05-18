bfadc2741bf6c0c66da8bfee7eb0aef059fadc01
crimsonpig <keith.stabbert@gmail.com>
2017-05-17 22:59:49 -0700

    Splitting up tests

--- finance-budgetitems/src/test/java/com/crimsonpig/finance/service/BudgetItemAmountServiceTest.java
+++ finance-budgetitems/src/test/java/com/crimsonpig/finance/service/BudgetItemAmountServiceTest.java
@@ -18,56 +18,6 @@ public class BudgetItemAmountServiceTest {
 	public void setUp(){
 		amountService = new BudgetItemAmountService();
 	}
-
-	@Test
-	public void testOneDayDifferenceAmount(){
-		int budgetItemNumberOfDays = 1;
-		int dateRangeNumberOfDays = 1; 
-		BigDecimal budgetItemAmount = new BigDecimal(500);
-		BigDecimal expectedAmount = new BigDecimal("500.00");
-		BigDecimal actualAmount = amountService.calculateAmountForDateRange(budgetItemNumberOfDays, dateRangeNumberOfDays, budgetItemAmount);
-		assertEquals(expectedAmount, actualAmount);
-	}
-	
-	@Test
-	public void test31DayDifferenceAmount31DayRange(){
-		int budgetItemNumberOfDays = 32;
-		int dateRangeNumberOfDays = 32; 
-		BigDecimal budgetItemAmount = new BigDecimal(500);
-		BigDecimal expectedAmount = new BigDecimal("500.00");
-		BigDecimal actualAmount = amountService.calculateAmountForDateRange(budgetItemNumberOfDays, dateRangeNumberOfDays, budgetItemAmount);
-		assertEquals(expectedAmount, actualAmount);
-	}
-	
-	@Test
-	public void test31DayDifferenceAmount15DayRange(){
-		int budgetItemNumberOfDays = 32;
-		int dateRangeNumberOfDays = 15; 
-		BigDecimal budgetItemAmount = new BigDecimal(500);
-		BigDecimal expectedAmount = new BigDecimal("234.38");
-		BigDecimal actualAmount = amountService.calculateAmountForDateRange(budgetItemNumberOfDays, dateRangeNumberOfDays, budgetItemAmount);
-		assertEquals(expectedAmount, actualAmount);
-	}	
-	
-	@Test
-	public void test31DayDifferenceAmount11DayRange(){
-		int budgetItemNumberOfDays = 32;
-		int dateRangeNumberOfDays = 11; 
-		BigDecimal budgetItemAmount = new BigDecimal(500);
-		BigDecimal expectedAmount = new BigDecimal("171.88");
-		BigDecimal actualAmount = amountService.calculateAmountForDateRange(budgetItemNumberOfDays, dateRangeNumberOfDays, budgetItemAmount);
-		assertEquals(expectedAmount, actualAmount);
-	}
-	
-	@Test
-	public void test32DayDifferenceAmount6DayRange(){
-		int budgetItemNumberOfDays = 32;
-		int dateRangeNumberOfDays = 6; 
-		BigDecimal budgetItemAmount = new BigDecimal(500);
-		BigDecimal expectedAmount = new BigDecimal("93.75");
-		BigDecimal actualAmount = amountService.calculateAmountForDateRange(budgetItemNumberOfDays, dateRangeNumberOfDays, budgetItemAmount);
-		assertEquals(expectedAmount, actualAmount);
-	}		
 	
 	@Test
 	public void testZeroExpenseItemAmountSameDay(){
